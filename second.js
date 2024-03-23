f94aa30f45651d5502d38192be76e2c09f119899
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-23 23:34:07 +0530

    use of some in built mosuls functions

--- second.js
+++ second.js
@@ -1,3 +1,17 @@
-let data = require("./index");
+// let mydata = require("./index");
+// const os = require("os");
+// console.log(data, os.freemem());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.release());
+// console.log(os.totalmem());
+// console.log(os.type());
+// console.log(os.uptime());
+// console.log(os.userInfo());
 
-console.log(data);
+const fs = require("fs");
+fs.readFile("myfile.html", "utf8", (err, data) => {
+  console.log(err, data);
+});
+console.log("hii");
